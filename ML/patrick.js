8e705db7ec6f8bf0af290bd43b82d47b846b367e
Patrick Lim <xirtap@yahoo.com>
2018-06-22 17:58:37 +0800

    Update patrick.js

--- ML/patrick.js
+++ ML/patrick.js
@@ -1,10 +1,7 @@
 const input = document.querySelector("input")
 const example = document.querySelector("#example")
 
-input.addEventListener("change", (e) => {
-const rgb = getRgb(e.target.value);
 const network = new brain.NeuralNetwork()
-console.log(rgb);
 network.train([
     { input: { r: 0.62, g: 0.72, b: 0.88 }, output: { light: 1 } },
     { input: { r: 0.1, g: 0.84, b: 0.72 }, output: { light: 1 } },
@@ -12,8 +9,12 @@ network.train([
     { input: { r: 0.74, g: 0.78, b: 0.86 }, output: { light: 1 } },
     { input: { r: 0.31, g: 0.35, b: 0.41 }, output: { dark: 1 } },
     { input: {r: 1, g: 0.99, b: 0}, output: { light: 1 } },
-    { input: {r: 1, g: 0.42, b: 0.52}, output: { dark: 1 } },
+    { input: {r: 1, g: 0.42, b: 0.52}, output: { dark: 1 } }
 ])
+
+input.addEventListener("change", (e) => {
+const rgb = getRgb(e.target.value);
+console.log(rgb);
 
 const result = brain.likely(rgb, network)
 console.log(result);
