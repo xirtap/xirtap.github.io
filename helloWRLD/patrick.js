0faf6a82f76d0d04314c5dc9dc45d9292d53f650
Patrick Lim <xirtap@yahoo.com>
2019-04-19 20:41:42 +0800

    Add files via upload

---
+++ helloWRLD/patrick.js
@@ -0,0 +1,16 @@
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    var map = L.Wrld.map("map", "da70b0d2173a3f3f5299df3692507b57", {
+    center: [position.coords.latitude,position.coords.longitude],
+    zoom: 16
+    });
+}
