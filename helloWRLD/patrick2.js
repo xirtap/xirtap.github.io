0fc284dd2f01fffcfdce3f9ac666b383e116ef95
Patrick Lim <xirtap@yahoo.com>
2019-04-20 20:20:05 +0800

    Update patrick2.js

--- helloWRLD/patrick2.js
+++ helloWRLD/patrick2.js
@@ -4,16 +4,37 @@ var map = L.Wrld.map("map", "da70b0d2173a3f3f5299df3692507b57", {
   });
   var marker = L.marker([1.3490, 103.8391]).addTo(map);  
 
-  var today = new Date();
-  var hours = today.getHours();
-  if (hours > 19) {
-    map.themes.setTime([L.Wrld.themes.time.Night]);
-  } else if (hours > 16) {
-    map.themes.setTime([L.Wrld.themes.time.Dusk]);
-  } else if (hours > 9) {
-    map.themes.setTime([L.Wrld.themes.time.Day]);
-  } else if (hours > 6) {
-    map.themes.setTime([L.Wrld.themes.time.Dawn]);
-  } else {
-    map.themes.setTime([L.Wrld.themes.time.Night]);
-  }
+    var today = new Date();
+    var hours = today.getHours();
+    if (hours > 19) {
+        map.themes.setTheme(
+            L.Wrld.themes.season.Summer,
+            L.Wrld.themes.time.Night,
+            L.Wrld.themes.weather.Clear
+        );
+    } else if (hours > 16) {
+        map.themes.setTheme(
+            L.Wrld.themes.season.Summer,
+            L.Wrld.themes.time.Dusk,
+            L.Wrld.themes.weather.Clear
+        );
+    } else if (hours > 9) {
+        map.themes.setTheme(
+            L.Wrld.themes.season.Summer,
+            L.Wrld.themes.time.Day,
+            L.Wrld.themes.weather.Clear
+        );
+    } else if (hours > 6) {
+        map.themes.setTheme(
+            L.Wrld.themes.season.Summer,
+            L.Wrld.themes.time.Dawn,
+            L.Wrld.themes.weather.Clear
+        );
+    } else {
+        map.themes.setTheme(
+            L.Wrld.themes.season.Summer,
+            L.Wrld.themes.time.Night,
+            L.Wrld.themes.weather.Clear
+        );
+    }
+
