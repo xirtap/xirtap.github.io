79669ee806b1db6830832efd4894e99141e50000
Patrick Lim <xirtap@yahoo.com>
2019-04-20 09:13:19 +0800

    Add files via upload

--- helloWRLD/patrick.js
+++ helloWRLD/patrick.js
@@ -10,7 +10,9 @@ function getLocation() {
 
 function showPosition(position) {
     var map = L.Wrld.map("map", "da70b0d2173a3f3f5299df3692507b57", {
-    center: [position.coords.latitude,position.coords.longitude],
-    zoom: 16
-    });
+        center: [position.coords.latitude,position.coords.longitude],
+        zoom: 16
+        });
+    var latlng = event.latlng;
+    var marker = L.marker(latlng).addTo(map);
 }
--- helloWRLD/patrick2.js
+++ helloWRLD/patrick2.js
@@ -2,3 +2,5 @@ var map = L.Wrld.map("map", "da70b0d2173a3f3f5299df3692507b57", {
     center: [1.3490, 103.8391],
     zoom: 16
   });
+  var latlng = event.latlng;
+  var marker = L.marker(latlng).addTo(map);  
