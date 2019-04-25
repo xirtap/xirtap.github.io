e6578c012c39504e9eddb7a74be48e799a952b15
Patrick Lim <xirtap@yahoo.com>
2019-04-25 09:23:34 +0800

    Add files via upload

--- helloWRLD/uat.js
+++ helloWRLD/uat.js
@@ -14,39 +14,42 @@ function showPosition(position) {
         zoom: 16
         });
     var marker = L.marker([position.coords.latitude,position.coords.longitude]).addTo(map);
-
     var today = new Date();
     var hours = today.getHours();
-    if (hours > 19) {
-        map.themes.setTheme(
-            L.Wrld.themes.season.Summer,
-            L.Wrld.themes.time.Night,
-            L.Wrld.themes.weather.Clear
-        );
-    } else if (hours > 16) {
-        map.themes.setTheme(
-            L.Wrld.themes.season.Summer,
-            L.Wrld.themes.time.Dusk,
-            L.Wrld.themes.weather.Clear
-        );
-    } else if (hours > 9) {
-        map.themes.setTheme(
-            L.Wrld.themes.season.Summer,
-            L.Wrld.themes.time.Day,
-            L.Wrld.themes.weather.Clear
-        );
-    } else if (hours > 6) {
-        map.themes.setTheme(
-            L.Wrld.themes.season.Summer,
-            L.Wrld.themes.time.Dawn,
-            L.Wrld.themes.weather.Clear
-        );
+    if (hours > 20) {
+        map.themes.setTime([L.Wrld.themes.time.Night]);
+    } else if (hours > 17) {
+        map.themes.setTime([L.Wrld.themes.time.Dusk]);
+    } else if (hours > 10) {
+        map.themes.setTime([L.Wrld.themes.time.Day]);
+    } else if (hours > 7) {
+        map.themes.setTime([L.Wrld.themes.time.Dawn]);
     } else {
-        map.themes.setTheme(
-            L.Wrld.themes.season.Summer,
-            L.Wrld.themes.time.Night,
-            L.Wrld.themes.weather.Clear
-        );
+        map.themes.setTime([L.Wrld.themes.time.Night]);
     }
 
+    function moveToLoc(Lat,Long) {
+        map.setView([Lat, Long], 18, {
+        headingDegrees: 30,
+        animate: true,
+        durationSeconds:5
+      });
+    }
+
+    function Cloud(searchEncoded){
+    $.ajax({
+    url: 'https://developers.onemap.sg/commonapi/search?searchVal='+searchEncoded+'&returnGeom=Y&getAddrDetails=Y&pageNum=1',
+    success: function(result){
+            var Lat = result.results[0].LATITUDE;
+            var Lng = result.results[0].LONGITUDE;
+            //document.getElementById("results").innerHTML = "Long & Lat: " + Lng + "," + Lat;
+            moveToLoc(Lat,Lng);
+        }});
+    }
+
+    function getsearchval(){
+        var search = document.getElementById('searchval').value;
+        var searchEncoded = encodeURIComponent(search);
+        Cloud(searchEncoded);
+    }    
 }
