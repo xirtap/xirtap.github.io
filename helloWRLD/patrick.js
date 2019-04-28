39110f5d7942561f5d57efc587cc55437051603e
Patrick Lim <xirtap@yahoo.com>
2019-04-28 15:28:15 +0800

    Add files via upload

--- helloWRLD/patrick.js
+++ helloWRLD/patrick.js
@@ -1,51 +1,71 @@
-var x = document.getElementById("demo");
+var homeLat=1.3490;
+var homeLng=103.8391;
+var apidelay=1000;
+var map = L.Wrld.map("map", "da70b0d2173a3f3f5299df3692507b57", {
+    center: [homeLat,homeLng],
+    zoom: 18
+    });
 
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        alert("Geolocation is not supported by this browser.");
     }
 }
 
+function moveToLoc(Lat,Long) {
+    map.setView([Lat, Long], 18, {
+        headingDegrees: 30,
+        animate: true,
+        durationSeconds:5
+    });
+
+    var mker = L.marker([Lat,Long]).addTo(map);
+}
+
 function showPosition(position) {
-    var map = L.Wrld.map("map", "da70b0d2173a3f3f5299df3692507b57", {
-        center: [position.coords.latitude,position.coords.longitude],
-        zoom: 16
-        });
+    moveToLoc(position.coords.latitude,position.coords.longitude);
     var marker = L.marker([position.coords.latitude,position.coords.longitude]).addTo(map);
 
     var today = new Date();
     var hours = today.getHours();
-    if (hours > 19) {
-        map.themes.setTheme(
-            L.Wrld.themes.season.Summer,
-            L.Wrld.themes.time.Night,
-            L.Wrld.themes.weather.Clear
-        );
-    } else if (hours > 18) {
-        map.themes.setTheme(
-            L.Wrld.themes.season.Summer,
-            L.Wrld.themes.time.Dusk,
-            L.Wrld.themes.weather.Clear
-        );
-    } else if (hours > 8) {
-        map.themes.setTheme(
-            L.Wrld.themes.season.Summer,
-            L.Wrld.themes.time.Day,
-            L.Wrld.themes.weather.Clear
-        );
-    } else if (hours > 6) {
-        map.themes.setTheme(
-            L.Wrld.themes.season.Summer,
-            L.Wrld.themes.time.Dawn,
-            L.Wrld.themes.weather.Clear
-        );
+    if (hours > 20) {
+        map.themes.setTime([L.Wrld.themes.time.Night]);
+    } else if (hours > 17) {
+        map.themes.setTime([L.Wrld.themes.time.Dusk]);
+    } else if (hours > 10) {
+        map.themes.setTime([L.Wrld.themes.time.Day]);
+    } else if (hours > 7) {
+        map.themes.setTime([L.Wrld.themes.time.Dawn]);
     } else {
-        map.themes.setTheme(
-            L.Wrld.themes.season.Summer,
-            L.Wrld.themes.time.Night,
-            L.Wrld.themes.weather.Clear
-        );
+        map.themes.setTime([L.Wrld.themes.time.Night]);
     }
 }
+
+function Cloud(searchEncoded){
+    var Lat=0;
+    var Lng=0;
+    var str1="https://developers.onemap.sg/commonapi/search?searchVal=";
+    var str2="&returnGeom=Y&getAddrDetails=Y&pageNum=1";
+    var apiurl=str1.concat(searchEncoded, str2);
+
+    $.ajax({
+    url: apiurl,
+    success: function(result){
+        Lat = result.results[0].LATITUDE;
+        Lng = result.results[0].LONGITUDE;
+    }});
+
+    setTimeout(function(){ moveToLoc(Lat,Lng); }, apidelay);
+}
+
+function getsearchval(){
+    var search = document.getElementById('searchval').value;
+    var searchEncoded = encodeURIComponent(search);
+    Cloud(searchEncoded);
+}    
+
+function balikkampung(){
+    setTimeout(function(){ moveToLoc(homeLat,homeLng); }, apidelay);
+}  
