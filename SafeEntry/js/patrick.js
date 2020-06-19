6971177d45b766bc97ef55d1385e8f840f21f875
Patrick Lim <xirtap@yahoo.com>
2020-06-19 15:36:54 +0800

    Add files via upload

--- SafeEntry/js/patrick.js
+++ SafeEntry/js/patrick.js
@@ -1,2 +1,4 @@
 gsap.from("#first", {duration: 1, x: 500});
+gsap.from("#first", {duration: 1.5, opacity: 0});
 gsap.from("#second", {duration: 1, x: -500});
+gsap.from("#second", {duration: 1.5, opacity: 0});
