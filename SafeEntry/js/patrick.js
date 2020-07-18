9c828cb3a9e32cb95b80cb159aecce8fec7a47e2
Patrick Lim <xirtap@yahoo.com>
2020-07-18 16:26:44 +0800

    Add files via upload

--- SafeEntry/js/patrick.js
+++ SafeEntry/js/patrick.js
@@ -2,3 +2,12 @@ gsap.from("#first", {duration: 1, x: 500});
 gsap.from("#first", {duration: 1.5, opacity: 0});
 gsap.from("#second", {duration: 1, x: -500});
 gsap.from("#second", {duration: 1.5, opacity: 0});
+
+
+var classname = this.document.getElementsByClassName("btn");
+
+for (var i = 0; i < classname.length; i++) { 
+  classname[i].addEventListener('mousedown', function () {
+    this.classList.add("press")
+  }, false);
+}
