b3c2c9c98db0755b1e5b2732e74958359696dc47
Patrick Lim <xirtap@yahoo.com>
2021-11-28 13:25:37 +0800

    Update Pat.js

--- JSUnity02/Pat.js
+++ JSUnity02/Pat.js
@@ -47,7 +47,7 @@ function draw() {
 
 
 // STEP 3: Get the classification!
-var x = setInterval(function gotResults(error, results) {
+function gotResults(error, results) {
   // Something went wrong!
   if (error) {
     console.error(error);
@@ -72,16 +72,16 @@ var x = setInterval(function gotResults(error, results) {
   }
   value=outresult;
   document.getElementById("demo").innerHTML = "Result is " + label + "; outresult is " + outresult + "; Value is " + value;
-  fl(label);
+  //fl(label);
   
-}, 1000);
+}
 
-
+/*
 var gameInstance = UnityLoader.instantiate("unityContainer", "Build/webgl.json", {onProgress: UnityProgress});
 
 function fl(outresult){
 	//gameInstance.SendMessage("Nav","ctrl",value);
 	gameInstance.SendMessage("Main Camera","ctrl",outresult);
 }
-
+*/
 
